85b883b20cad877ce2b4c39f6d4d066df926ad84
BuangMo <bmolelekwa@gmail.com>
2024-05-20 11:22:27 +0200

    cover pulse, noise effect and multiple false targets function added

--- CodeFiles/ea_es.py
+++ CodeFiles/ea_es.py
@@ -17,6 +17,7 @@ class Initialiser:
     num_pulses: int = 32          # the number of pulses for the transmitted waveform
     volume: float = 1             # the volume of the sound output
     duration: int = 2             # the duration to play the signal
+    Ts: float = 1 / fs            # the sampling period of the sound card
 
 class TXSignal(Initialiser):
     def generateTXSignal(self, operation):
@@ -86,6 +87,7 @@ class TXSignal(Initialiser):
         recurring_delay = int(self.pw_samples / (4 * delay_factor))
         append_pulse = True
         track_range_gate = init_delay + recurring_delay
+        track_pulse_count = 0
 
         # create zeros samples for initial delay and recurring delay
         init_delay_zeros = np.zeros(init_delay)
@@ -106,32 +108,44 @@ class TXSignal(Initialiser):
         rgpo_sig = np.append(rgpo_sig, init_delay_zeros)
 
         # delay the rgpo by the number of pulses to make it realistic
-        delay_pulses = rdom.randint(4, 10)
+        delay_pulses = 5
         print(f'Delaying the range gate pull-off signal by {delay_pulses} pulses')
-        for i in range(delay_pulses):
-            rgpo_sig = np.append(rgpo_sig, self.pri_zeros)
+        rgpo_sig = np.append(rgpo_sig, np.zeros(delay_pulses * len(self.pri_zeros)))
 
+        # capture the AGC
+        capture_pulses = 9
+        print(f'Capturing the AGC with {capture_pulses} pulses')
+        for ii in range(delay_pulses, capture_pulses): 
+            rgpo_sig = np.append(rgpo_sig, pulse)
+            rgpo_sig = np.append(rgpo_sig, self.lstn_zeros)
+
+        j = 0
         # append the pulse signal to rgpo_sig while gradually pulling off from the range gate
-        while append_pulse:
+        while append_pulse or (track_pulse_count <= (self.num_pulses - delay_pulses - capture_pulses)):
             rgpo_sig = np.append(rgpo_sig, pulse)
             rgpo_sig = np.append(rgpo_sig, self.lstn_zeros)
             rgpo_sig = np.append(rgpo_sig, recur_delay_zeros)
+            j += 1
 
             # update track_range_gate variable and verify its within the gate
+            #print(track_range_gate)
             track_range_gate += recurring_delay
             append_pulse = True if track_range_gate < self.total_samples else False
+            track_pulse_count += 1
 
+        print(f'took {j} to walk away from the range gate')
         # add the zeros to match expected size
         rem_samples = int(self.fs * self.duration) - len(rgpo_sig)
         rem_zeros = np.zeros(rem_samples)
         rgpo_sig = np.append(rgpo_sig, rem_zeros)
+        self.plotter(rgpo_sig, 1)
         rgpo_sig = np.array(rgpo_sig).astype(np.float32)
 
         return rgpo_sig
 
     def coverPulse(self, cover_fc):
         '''generates a cover pulse signal to prevent operation of the sonar'''
-        min_distance = self.R_min + 0.2
+        min_distance = self.R_min + 0.5
         cover_sig = []
 
         # shorten the delay and listening times to cater for a longer cover pulse
@@ -147,33 +161,202 @@ class TXSignal(Initialiser):
         # generate an LFM pulse with these parameters
         adj_pulse = chirp(
             np.linspace(0, adj_pulse_width, adj_pw_samples), 
-            f0=self.fc,
+            f0=cover_fc,
             f1=cover_fc+self.bwidth, 
             t1=adj_pulse_width, 
             method='linear'
         ).astype(np.float32)
 
         # generate the cover signal
-        cover_sig = np.append(cover_sig, adj_delay_samples)
+        samps = int((self.total_samples - adj_pw_samples) / 2)
+        cover_sig = np.append(cover_sig, self.delay_samples)
+        cover_sig = np.append(cover_sig, np.zeros(864))
 
         # delay generation of cover pulses to mimic DRFM reception, storage, and retransmission
-        delay_pulses = rdom.randint(3, 6)
+        delay_pulses = 8
         print(f'The cover pulse to be generated after the {delay_pulses}th LFM pulse')
-        for i in range(delay_pulses):
-            cover_sig = np.append(cover_sig, self.pri_zeros)
+        cover_sig = np.append(cover_sig, np.zeros(delay_pulses * len(self.pri_zeros)))
 
         # generate cover pulse signals for the remaining pulses
-        for i in range(self.num_pulses - delay_pulses):
+        for i in range(delay_pulses, self.num_pulses):
             cover_sig = np.append(cover_sig, adj_pulse)
-            cover_sig = np.append(cover_sig, adj_lstn_samples)
+            cover_sig = np.append(cover_sig, np.zeros(int(2 * samps)))
 
-        cover_sig = np.append(cover_sig, adj_delay_samples)
-        cover_sig = np.append(cover_sig, np.zeros(2 * adj_samples))
-        print(len(adj_delay_samples))
-        print(len(self.delay_samples))
+        #cover_sig = np.append(cover_sig, adj_delay_samples)
+        cover_sig = np.append(cover_sig, np.zeros(int(self.duration * self.fs - len(cover_sig))))
         cover_sig = np.array(cover_sig).astype(np.float32)
 
         return cover_sig
+    
+    def velocity_deception(self, vgpo_fc):
+        '''Generates a velocity gate pull-off signal'''
+        vgpo_sig = []
+        fshift = 0.25        # Hz
+        fshift_range = 2.3  # the range at which fshift is placed + minimum
+        counter = 2         # a multiplier for the frequency increment
+
+        # generate a pulse signal shifted in frequency
+        fshift_pulse = chirp(
+            np.linspace(0, self.pulse_width, self.pw_samples), 
+            f0=vgpo_fc,
+            f1=vgpo_fc+self.bwidth, 
+            t1=self.pulse_width, 
+            method='linear'
+        ).astype(np.float32)
+
+        # delay transmission to mimic DRFM signal capture, storage, modification and retransmission
+        delay_trans = rdom.randint(3,6)
+        print(f'The vgpo signal is delayed up until the {delay_trans}th pulse.')
+        vgpo_sig = np.append(vgpo_sig, self.delay_samples)
+        for i in range(delay_trans):
+            vgpo_sig = np.append(vgpo_sig, self.pri_zeros)
+
+        # mimic capturing of the AGC at a certain range
+        fshift_samples = self.pw_samples + ceil(((2 * fshift_range) / self.sound_speed) / (1 / self.fs ))
+        fshift_zeros = np.zeros(fshift_samples)
+        vgpo_sig = np.append(vgpo_sig, fshift_zeros)
+        capturing_agc = rdom.randint(3, 5)
+        print(f'Capturing the AGC with {capturing_agc} pulses of the same frequency')
+        for i in range(capturing_agc):
+            vgpo_sig = np.append(vgpo_sig, fshift_pulse)
+            vgpo_sig = np.append(vgpo_sig, self.lstn_zeros)
+
+        # calculate the maximum unambiguous velocity of the system
+        v_max = self.sound_speed**2 / (8 * self.fc * self.R_max)
+        v_shift = (self.sound_speed * fshift) / (2 * vgpo_fc)
+        v_update = v_shift
+
+        # increase the frequency shift to mimic a moving away target
+        while v_update < v_max:
+            # generate the pulse signal with a different frequency
+            fshift_pulse = chirp(
+                np.linspace(0, self.pulse_width, self.pw_samples), 
+                f0=vgpo_fc+(counter*fshift),
+                f1=vgpo_fc+self.bwidth+(counter*fshift), 
+                t1=self.pulse_width, 
+                method='linear'
+            ).astype(np.float32)
+            
+            # append the doppler shifted pulse to the signal
+            vgpo_sig = np.append(vgpo_sig, fshift_pulse)
+            vgpo_sig = np.append(vgpo_sig, self.lstn_zeros)
+
+            # update the variables for a frequency increment
+            counter += 1
+            v_update += v_shift
+        
+        # append all the remaining zeros
+        rem_zeros = np.zeros(int((self.duration * self.fs) - len(vgpo_sig)))
+        vgpo_sig = np.append(vgpo_sig, rem_zeros)
+        vgpo_sig = np.array(vgpo_sig).astype(np.float32)
+
+        return vgpo_sig
+    
+    def mft(self):
+        '''Generates a PRI with multiple false targets'''
+        # define the pulse characteristics you want in your false target
+        # these have to be within the PRI of your pulse
+        mft_sig = []
+        pri_mft_sig = []
+        track_samples = 0
+
+        # adding the delay before the first false target is added 
+        pulse_delay = 4
+        pulse_zeros = np.zeros(int(pulse_delay * len(self.pri_zeros)))
+        mft_sig = np.append(mft_sig, self.delay_samples)
+        mft_sig = np.append(mft_sig, pulse_zeros)
+
+        # adding the delay before introducing a false target pulse
+        sample_delay = int((self.lstn_samples - self.pw_samples) / 2)
+        sample_zeros = np.zeros(sample_delay)
+        pri_mft_sig = np.append(pri_mft_sig, sample_zeros)
+
+        # defining 1st target (stationery)
+        pulse = chirp(
+            np.linspace(0, self.pulse_width, self.pw_samples), 
+            f0=self.fc,
+            f1=self.fc+self.bwidth, 
+            t1=self.pulse_width, 
+            method='linear'
+        ).astype(np.float32)
+        pri_mft_sig = np.append(pri_mft_sig, np.zeros(self.pw_samples)) # should be pulse
+        
+        another_delay_samples = int(sample_delay / 2)
+        ads_zeros = np.zeros(another_delay_samples)
+        pri_mft_sig = np.append(pri_mft_sig, ads_zeros)
+        track_samples = sample_delay + self.pw_samples + another_delay_samples
+
+        # adding the second false target pulse (moving)
+        '''pulse = chirp(
+            np.linspace(0, self.pulse_width, self.pw_samples), 
+            f0=self.fc+20,
+            f1=self.fc+self.bwidth+20, 
+            t1=self.pulse_width, 
+            method='linear'
+        ).astype(np.float32)'''
+        t = np.linspace(0, self.pulse_width, self.pw_samples)
+        chirp_slope = (12e3-9e3)/self.pulse_width
+        inst_freq = 10.5e3 + (12e3-9e3)/2*(2*t/self.pulse_width - 1)
+        phase = 2 * np.pi * np.cumsum(inst_freq * t)
+
+        doppler_shift = (2 * 0.05)*  10.5e3
+        shifted_phase = np.angle(self.pulse) + np.pi/2.5
+
+        pulse = np.cos(shifted_phase)
+        pri_mft_sig = np.append(pri_mft_sig, pulse)
+        pri_mft_sig = np.append(pri_mft_sig, ads_zeros)
+        track_samples += self.pw_samples + another_delay_samples
+
+        # the number of samples at this point should equal a PRI
+        if track_samples != self.total_samples:
+            print(f'The number of pri samples and mft pri samples not equal. track_samples = {track_samples}')
+            exit(1)
+
+        # when you are done with your desired false targets, add them to your stream of pulses
+        # make sure you add some delay of pulses before adding them
+        for ii in range(pulse_delay, self.num_pulses):
+            mft_sig = np.append(mft_sig, pri_mft_sig)
+
+        mft_sig = np.append(mft_sig, self.delay_samples)
+        mft_sig = np.array(mft_sig).astype(np.float32)
+
+        return mft_sig
+    
+    def noise_effect(self, f0, noise_bwidth, order=6):
+        '''generates a signal with varying bandwidth''' 
+        noise_sig = []
+        noise_sig = np.append(noise_sig, self.delay_samples)
+
+        # generate the white noise signal
+        noise = np.random.randn(self.num_pulses * self.total_samples)
+
+        # design a bandpass filter
+        from scipy.signal import lfilter, butter
+        nyquist_rate = self.fs / 2
+        lowcut = (f0 - noise_bwidth / 2) / nyquist_rate 
+        highcut = (f0 + noise_bwidth / 2) / nyquist_rate
+        print(f'The lower and upper cutoff frequencies are {lowcut} and {highcut}')
+        b, a = butter(order, [lowcut, highcut], btype='bandpass')
+
+        # apply the filter
+        filtered_noise = lfilter(b, a, noise)
+
+        plt.subplot(111)
+        F_X = np.fft.fftshift(np.fft.fft(filtered_noise, 1024))
+        freq = np.arange(-self.fs / 2 , self.fs / 2, self.fs / len(F_X))
+        plt.plot(freq / 1000, np.abs(F_X))
+        plt.xlabel("Frequency (kHz)")
+        plt.ylabel('Magnitude')
+        plt.xticks(np.arange(-self.fs / (2 * 1000), self.fs / (2 * 1000), 2))
+        plt.grid(True)
+        plt.show()
+
+        # append to the noise signal 
+        noise_sig = np.append(noise_sig, filtered_noise)
+        noise_sig = np.append(noise_sig, self.delay_samples)
+        noise_sig = np.array(noise_sig).astype(np.float32)
+
+        return noise_sig
 
     def plotter(self, f_x, plot_sel=2):
         '''Plots the time series and frequency spectrums of the given signal'''
--- CodeFiles/elb780_2.py
+++ CodeFiles/elb780_2.py
@@ -1,5 +1,6 @@
 from ea_es import TXSignal
 import numpy as np
+from math import ceil
 from matplotlib import pyplot as plt
 import pyaudio, queue, time
 from threading import Thread
@@ -76,14 +77,14 @@ class ELB780_2:
             self.p.terminate()
             print(f'A {type(error).__name__} has occured')
 
-    def terminate_pa( self ):
+    def terminate_pa(self):
         '''Terminates the pyaudio binding to portaudio'''
         self.p.terminate()
 
     def pulse_compression(self):
         '''Applies the matched filter to the received data'''
         print("Applying the matched filter")
-        self.tx_signal.plotter(self.rcv, 1)
+        #self.tx_signal.plotter(self.rcv, 1)
         self.mfiltered = np.convolve(
             hilbert(self.rcv), 
             np.conj(np.flipud(hilbert(self.tx_signal.pulse))), 
@@ -93,16 +94,19 @@ class ELB780_2:
 
     def find_1st_peak(self):
         stop_search = int(self.mfiltered.size / 2)
-        peaks, _ = find_peaks(np.abs(self.mfiltered[:stop_search]), height=10)
+        peaks, _ = find_peaks(np.abs(self.mfiltered[:stop_search]), height=4)
         trace_peak = 0
 
         # run through the peaks to find the difference in samples between consecutive peaks
         for i in range(peaks.size):
             distance = peaks[i + 1] - peaks[i]
-            if distance > 25:
+            if distance > 20:
                 trace_peak = i
                 break
-        
+
+        if peaks[trace_peak] < 22e3:
+            print(peaks[trace_peak])
+            exit(444)
         return peaks[trace_peak]
 
     def data_matrix(self):
@@ -126,36 +130,194 @@ class ELB780_2:
         #integrate the pulses
         dt = np.mean(self.pri_data.T, axis=0)
         analytic_sigg = np.abs(dt)
-        self.tx_signal.plotter(analytic_sigg, 2)
+        self.tx_signal.plotter(analytic_sigg, 1)
+
+        t = np.arange(len(self.pri_data.T[0])) / self.tx_signal.fs
+        i = 1
+        path = 'C:/Users/Buang/Documents/python_projects/tukkies/00 ELB780/2024/02 Assignment2/Results/03 Cover Pulse/Averaged/'
+        self.file_handling(path)
+        print('Writing the figures to the folder...')
+        for ii in self.pri_data.T:
+            lable = 'pulse ' + str(i)
+            i += 1
+            fig, ax = plt.subplots(nrows=1, ncols=1)
+            ax.plot(t, np.abs(ii), label=lable)
+            ax.legend()
+            ax.set(xlabel='time (s)', ylabel='Magnitude')
+            ax.grid(True)
+            fig.savefig(path + lable + '.png')
+            #plt.show()
+            plt.close(fig)
+
+    def file_handling(self, path_to_folder):
+        import pathlib
+        import shutil
+
+        try:
+            # delete the folder
+            folder_path = pathlib.Path(path_to_folder)
+            shutil.rmtree(path_to_folder, ignore_errors=True)
+            print('Folder deleted')
+            # create a folder
+            folder_path.mkdir()
+        except FileNotFoundError:
+            print("Folder doesn't exist")
+            folder_path.mkdir()
 
     def signal_construction(self):
         '''Constructs the waveform to survey the environment'''
         esupport_sig = self.tx_signal.generateTXSignal(self.operation)
-        smps = np.arange(esupport_sig.size)
+        smps = np.arange(esupport_sig.size) / 1000
         plt.subplot(111)
 
         # construct the bytes signal based on the value of the eattack_select variable
         if self.eattack_select == 1:         # range gate
             eattack_sig = self.tx_signal.generate_rgpo_sig()
-            plt.plot(smps, eattack_sig)
+            #plt.plot(smps, eattack_sig, label='rgpo signal')
             output_bytes = np.column_stack((esupport_sig, eattack_sig))
             self.output_bytes = (self.tx_signal.volume * output_bytes).tobytes()
         elif self.eattack_select == 2:       # velocity gate pull-off
-            pass
+            eattack_sig = self.tx_signal.velocity_deception(3e3)
+            #plt.plot(smps, eattack_sig)
+            output_bytes = np.column_stack((esupport_sig, eattack_sig))
+            self.output_bytes = (self.tx_signal.volume * output_bytes).tobytes()
         elif self.eattack_select == 3:       # cover pulse
-            eattack_sig = self.tx_signal.coverPulse(20e3)
-            plt.plot(smps, eattack_sig)
+            eattack_sig = self.tx_signal.coverPulse(9e3)
+            #self.tx_signal.plotter(eattack_sig, 5)
+            plt.plot(smps, eattack_sig, label='cover pulse')
             output_bytes = np.column_stack((esupport_sig, eattack_sig))
             self.output_bytes = (self.tx_signal.volume * output_bytes).tobytes()
         elif self.eattack_select == 4:       # multiple false targets
-            pass
+            eattack_sig = self.tx_signal.mft()
+            #self.tx_signal.plotter(eattack_sig, 5)
+            #plt.plot(smps, eattack_sig, label='mft signal')
+            output_bytes = np.column_stack((esupport_sig, eattack_sig))
+            self.output_bytes = (self.tx_signal.volume * output_bytes).tobytes()
+        elif self.eattack_select == 5:       # noise with bandwidth effects
+            eattack_sig = self.tx_signal.noise_effect(10.5e3, 3e3)
+            #self.tx_signal.plotter(eattack_sig, 5)
+            #plt.plot(smps, eattack_sig, label='noise signal')
+            output_bytes = np.column_stack((esupport_sig, eattack_sig))
+            self.output_bytes = (self.tx_signal.volume * output_bytes).tobytes()
         else:
             self.output_bytes = (self.tx_signal.volume * esupport_sig).tobytes()
 
-        plt.plot(smps, esupport_sig)
-        plt.xlabel('samples')
+        plt.plot(smps, esupport_sig, label='sonar signal')
+        plt.legend()
+        plt.xlabel('samples (k)')
         plt.ylabel('amplitude')
         plt.grid()
+        plt.show()
+
+    def doppler_processing(self):
+        '''Applying the Doppler filter to the data matrix'''
+        print( "Applying the Doppler filter to the data matrix" )
+
+        # calculate range and Doppler resolutions
+        delta_r = self.tx_signal.sound_speed / (2 * self.tx_signal.bwidth)
+        delta_v = self.tx_signal.sound_speed / (2 * ((self.tx_signal.bwidth / 2) + self.tx_signal.fc) * (self.tx_signal.pw_samples * self.tx_signal.Ts))
+
+        # calculate the number of range and Doppler Cells
+        Nr = int(ceil(2 * self.tx_signal.R_max / delta_r))
+        Nv = int(ceil(2 * self.tx_signal.fs / delta_v))
+
+        # generate range and Doppler FFT grids
+        r_grid = np.linspace(0, Nr*delta_r, Nr, endpoint=False)
+        v_grid = np.linspace(-Nv/2, Nv/2-1, Nv)*delta_v
+        num_bins = 128
+        print(self.pri_data.shape)
+
+        #generate range-Doppler map
+        self.doppler_spectrum = np.zeros((self.tx_signal.total_samples, num_bins), dtype='complex_')
+        for i in range(self.tx_signal.total_samples):
+            self.doppler_spectrum[i,:] = np.fft.fftshift(np.fft.fft(self.pri_data[i,:], num_bins))
+
+        # generate velocity grid
+        lambda_c = self.tx_signal.sound_speed / ((self.tx_signal.bwidth / 2) + self.tx_signal.fc)
+        v_max = lambda_c / (4 * ((self.tx_signal.pw_samples + self.tx_signal.lstn_samples) * self.tx_signal.Ts))
+        v_grid = np.linspace(-v_max, v_max, Nv)
+
+        # normalise the results of the range doppler plot
+        max_value = np.max(np.abs(self.doppler_spectrum))
+        self.doppler_spectrum1 = np.abs(self.doppler_spectrum) / max_value
+
+        doppler = np.log10(np.abs(self.doppler_spectrum.T))
+
+        self.extent = [r_grid[0], r_grid[-1]/2, -v_grid[-1], -v_grid[0]]
+        plt.imshow(np.abs(self.doppler_spectrum1.T), aspect='auto', cmap='jet', extent=self.extent)
+        plt.xlabel('Range (m)')
+        plt.xticks(np.arange(0, 8, 1))
+        plt.ylabel('Velocity (m/s)')
+        #plt.title('Range-Doppler map')
+        plt.colorbar(label='Intensity Level')
+        plt.show()
+
+    def cfar2Ddetection(self, guard_band, training_cells, threshold_factor):
+        ''' applies the 2D CFAR filter to the data'''
+        doppler_spectrum = self.doppler_spectrum1
+        print(self.doppler_spectrum.shape)
+        num_rows, num_cols = doppler_spectrum.shape
+        threshold_map = np.zeros_like(doppler_spectrum)
+        away_edges = 2 * (training_cells + guard_band) + 1
+
+        for i in range(num_rows):
+            for j in range(num_cols):
+                # define the region of interest
+                start_row_a = max(0, i - guard_band - training_cells)
+                end_row_a = min(num_rows, i + guard_band + training_cells + 1)
+                start_col_a = max(0, j - guard_band - training_cells)
+                end_col_a = min(num_cols, j + guard_band + training_cells + 1)
+
+                # define the region of delete
+                start_row_del = max(0, i - guard_band)
+                end_row_del = min(num_rows, i + guard_band + 1)
+                start_col_del = max(0, j - guard_band)
+                end_col_del = min(num_cols, j + guard_band + 1)
+
+                # when away from the left edge
+                if end_col_a > away_edges:
+                    start_col_del = training_cells
+                    end_col_del = 2 * training_cells + guard_band + 1
+
+                # when away from the top row
+                if end_row_a > away_edges:
+                    start_row_del = training_cells
+                    end_row_del = 2 * training_cells + guard_band + 1
+
+                # caculation of the local mean excluding the celss under test (CUT)
+                local_data_a = doppler_spectrum[start_row_a:end_row_a, start_col_a:end_col_a]
+                mask = np.ones_like(local_data_a, dtype=bool)
+                mask[start_row_del:end_row_del, start_col_del:end_col_del] = False
+                local_data = local_data_a[mask]
+
+                local_mean = np.mean(local_data)
+
+                # calculation of the local standard deviation
+                local_std = np.std(local_data)
+
+                # calculate the threshold
+                threshold = local_mean * threshold_factor #* local_std
+
+                # if the cell value is greater than the threshold, mark it as 1, else 0
+                if doppler_spectrum[i, j] < threshold:
+                    threshold_map[i, j] = 100
+                else:
+                    threshold_map[i, j] = 0
+
+        plt.figure(figsize=(10, 5))
+        plt.subplot(211)
+        plt.imshow(doppler_spectrum.T, cmap='jet', aspect='auto', extent=self.extent)
+        plt.title("(a)", loc='left')
+        plt.ylabel('Velocity (m/s)')
+        plt.colorbar(label='Intensity Level')
+        plt.xticks(np.arange(0, 7, 1))
+
+        plt.subplot(212)
+        plt.imshow(threshold_map.T, cmap='Greys', aspect='auto', extent=self.extent)
+        plt.title("(b)", loc='left')
+        plt.ylabel('Velocity (m/s)')
+        plt.xlabel('Range (m)')
+        plt.colorbar(label='Intensity Level')
         plt.show()
 
     def testing(self):
@@ -171,7 +333,7 @@ def main():
     obj.signal_construction()
     
     # defines the threads for playing and recording
-    '''ithread = Thread(target=obj.listener)
+    ithread = Thread(target=obj.listener)
     othread = Thread(target=obj.emitter)
     # starts the playing and recording threads
     ithread.start()
@@ -184,7 +346,10 @@ def main():
 
     obj.terminate_pa()
     obj.pulse_compression()
-    obj.data_matrix()'''
+    obj.data_matrix()
+
+    obj.doppler_processing()
+    #obj.cfar2Ddetection(3, 5, 20.745)
 
 if __name__ == '__main__':
     main()
